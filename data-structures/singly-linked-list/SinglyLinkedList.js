d34cadb5707965c516da2cc706bf643663ba6606
kkauma <kylekauma@gmail.com>
2024-07-31 13:55:20 -0500

    add logic for removing a node from the end of the list

--- data-structures/singly-linked-list/SinglyLinkedList.js
+++ data-structures/singly-linked-list/SinglyLinkedList.js
@@ -24,4 +24,26 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (!this.length) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
