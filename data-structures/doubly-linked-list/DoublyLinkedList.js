83888294cac918d0c0d11605d40f98ed2230ab5a
kkauma <kylekauma@gmail.com>
2024-08-01 13:58:25 -0500

    write logic for constructor and push method

--- data-structures/doubly-linked-list/DoublyLinkedList.js
+++ data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -5,3 +5,26 @@ class Node {
     this.prev = null;
   }
 }
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+  }
+}
