0c5feec8364a52ad41cb7493c614c922e2089470
kkauma <kylekauma@gmail.com>
2024-08-02 16:16:00 -0500

    write logic to add a new node to the beginning of the list

--- data-structures/doubly-linked-list/DoublyLinkedList.js
+++ data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -61,4 +61,19 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
