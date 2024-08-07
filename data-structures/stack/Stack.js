92a4700ced11b161045f91498fcc0afa35315661
kkauma <kylekauma@gmail.com>
2024-08-06 19:26:35 -0500

    start writing logic to implement a stack

---
+++ data-structures/stack/Stack.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+    }
+  }
+}
