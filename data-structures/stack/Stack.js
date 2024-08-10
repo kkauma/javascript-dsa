295ca31ff59014bb4a21357f8534382ac70175e0
kkauma <kylekauma@gmail.com>
2024-08-10 16:32:24 -0500

    write logic for removing a node from the stack

--- data-structures/stack/Stack.js
+++ data-structures/stack/Stack.js
@@ -24,4 +24,16 @@ class Stack {
     }
     return ++this.size;
   }
+
+  pop() {
+    if (!this.first) return null;
+
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
 }
