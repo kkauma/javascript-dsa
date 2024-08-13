f45a7a5e09d504e7cf588776444d27496d6cd7bf
kkauma <kylekauma@gmail.com>
2024-08-13 17:30:24 -0500

    add node class and constructor for queue class

---
+++ data-structures/queue/Queue.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = first;
+    this.last = last;
+    this.size = 0;
+  }
+}
