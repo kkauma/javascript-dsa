18d333317831df7000aa7ed0ac562ab5c3970c1b
kkauma <kylekauma@gmail.com>
2024-08-13 17:41:10 -0500

    write logic for adding and removing a node from a queue

--- data-structures/queue/Queue.js
+++ data-structures/queue/Queue.js
@@ -7,8 +7,32 @@ class Node {
 
 class Queue {
   constructor() {
-    this.first = first;
-    this.last = last;
+    this.first = null;
+    this.last = null;
     this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
   }
 }
