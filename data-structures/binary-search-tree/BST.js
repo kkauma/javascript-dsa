3c9eb049647d7c0374c1605f3b25d7fa8a3a3d02
kkauma <kylekauma@gmail.com>
2024-08-13 19:57:13 -0500

    write logic for finding a node in a BST

--- data-structures/binary-search-tree/BST.js
+++ data-structures/binary-search-tree/BST.js
@@ -39,4 +39,22 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (!this.root) return undefined;
+
+    let current = this.root,
+      found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return current;
+  }
 }
