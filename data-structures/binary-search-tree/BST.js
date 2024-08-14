9de981a674220918a46e4d8aee944e5a45be1114
kkauma <kylekauma@gmail.com>
2024-08-13 19:46:39 -0500

    write logic for inserting a new node in the binary search tree

--- data-structures/binary-search-tree/BST.js
+++ data-structures/binary-search-tree/BST.js
@@ -10,4 +10,33 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
