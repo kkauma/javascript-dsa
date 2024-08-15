ac22d96308a47bd608d34253a0de16e119c44590
kkauma <kylekauma@gmail.com>
2024-08-14 21:21:20 -0500

    implement breadth first search method on a tree

--- data-structures/binary-search-tree/BST.js
+++ data-structures/binary-search-tree/BST.js
@@ -60,4 +60,20 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  BFS() {
+    let node = this.root,
+      data = [],
+      queue = [];
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
